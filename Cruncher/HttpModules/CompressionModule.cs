cde7a77c7ed96865e96f5b9381d683281a13c98b
JimBobSquarePants <james_south@hotmail.com>
2012-07-15 02:13:47 +0100

    Stylecop tidy of compression module.

--- Cruncher/HttpModules/CompressionModule.cs
+++ Cruncher/HttpModules/CompressionModule.cs
@@ -11,10 +11,7 @@ namespace Cruncher.HttpModules
 {
     #region Using
     using System;
-    using System.IO;
     using System.IO.Compression;
-    using System.Text;
-    using System.Text.RegularExpressions;
     using System.Web;
     using System.Web.UI;
     using Cruncher.Config;
@@ -172,24 +169,13 @@ namespace Cruncher.HttpModules
                 context.Request.HttpMethod == "GET")
             {
                 CompressResponse(context);
-
-                // TODO: Do we need this anymore??
-                //if (PeachConfiguration.CompressWebResource)
-                //{
-                //    context.Response.Filter = new WebResourceFilter(context.Response.Filter);
-                //    WillCompressResponse = true;
-                //}
             }
-            //else if (!PeachConfiguration.CompressWebResource && context.Request.Path.Contains("WebResource.axd"))
-            //{
-            //    context.Response.Cache.SetExpires(DateTime.Now.AddDays(30));
-            //}
         }
 
         #region Static
 
         /// <summary>
-        /// 
+        /// Handles any unhandled exceptions that take place whilst compressing the response.
         /// </summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">An <see cref="T:System.EventArgs">EventArgs</see> that contains the event data.</param>
@@ -197,7 +183,7 @@ namespace Cruncher.HttpModules
         {
             HttpContext context = ((HttpApplication)sender).Context;
 
-            // If this CompressionModule will be compressing the response and an unhandled exception
+            // If this CompressionModule is compressing the response and an unhandled exception
             // has occurred, remove the WebResourceFilter as that will cause garbage characters to
             // be sent to the browser instead of a yellow screen of death.
             if (WillCompressResponse)
@@ -206,261 +192,8 @@ namespace Cruncher.HttpModules
                 WillCompressResponse = false;
             }
         }
-
         #endregion
         #endregion
-
         #endregion
-
-        /// <summary>
-        /// The web resource filter.
-        /// </summary>
-        private class WebResourceFilter : Stream
-        {
-            #region Constants and Fields
-
-            /// <summary>
-            /// The _sink.
-            /// </summary>
-            private readonly Stream sink;
-
-            #endregion
-
-            #region Constructors and Destructors
-
-            /// <summary>
-            /// Initializes a new instance of the <see cref="WebResourceFilter"/> class.
-            /// </summary>
-            /// <param name="sink">
-            /// The sink stream.
-            /// </param>
-            public WebResourceFilter(Stream sink)
-            {
-                this.sink = sink;
-            }
-
-            #endregion
-
-            #region Properties
-
-            /// <summary>
-            /// Gets a value indicating whether CanRead.
-            /// </summary>
-            public override bool CanRead
-            {
-                get
-                {
-                    return true;
-                }
-            }
-
-            /// <summary>
-            /// Gets a value indicating whether CanSeek.
-            /// </summary>
-            public override bool CanSeek
-            {
-                get
-                {
-                    return true;
-                }
-            }
-
-            /// <summary>
-            /// Gets a value indicating whether CanWrite.
-            /// </summary>
-            public override bool CanWrite
-            {
-                get
-                {
-                    return true;
-                }
-            }
-
-            /// <summary>
-            /// Gets Length.
-            /// </summary>
-            public override long Length
-            {
-                get
-                {
-                    return 0;
-                }
-            }
-
-            /// <summary>
-            /// Gets or sets Position.
-            /// </summary>
-            public override long Position { get; set; }
-
-            #endregion
-
-            #region Public Methods
-
-            /// <summary>
-            /// The close.
-            /// </summary>
-            public override void Close()
-            {
-                this.sink.Close();
-            }
-
-            /// <summary>
-            /// Evaluates the replacement for each link match.
-            /// </summary>
-            /// <param name="match">
-            /// The match.
-            /// </param>
-            /// <returns>
-            /// The evaluator.
-            /// </returns>
-            public string Evaluator(Match match)
-            {
-                string relative = match.Groups[1].Value;
-                string absolute = HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority);
-
-                // TODO: DO we need this?
-                string root = VirtualPathUtility.ToAbsolute("~/");
-
-                //string root = VirtualPathUtility.ToAbsolute(PeachConfiguration.VirtualPath);
-                string replaced = string.Format("{0}js.axd?path={1}", root, HttpUtility.UrlEncode(absolute + relative));
-
-                return match.Value.Replace(relative, replaced);
-            }
-
-            /// <summary>
-            /// When overridden in a derived class, clears all buffers for this stream and causes any buffered data 
-            /// to be written to the underlying device.
-            /// </summary>
-            /// <exception cref="T:System.IO.IOException">
-            /// An I/O error occurs.
-            /// </exception>
-            public override void Flush()
-            {
-                this.sink.Flush();
-            }
-
-            /// <summary>
-            /// When overridden in a derived class, reads a sequence of bytes from the current stream and advances
-            /// the position within the stream by the number of bytes read.
-            /// </summary>
-            /// <param name="buffer">
-            /// An array of bytes. When this method returns, the buffer contains the specified byte array with the values
-            /// between <paramref name="offset"/> and (<paramref name="offset"/> + <paramref name="count"/> - 1) replaced 
-            /// by the bytes read from the current source.
-            /// </param>
-            /// <param name="offset">
-            /// The zero-based byte offset in <paramref name="buffer"/> at which to begin storing the data read from the 
-            /// current stream.
-            /// </param>
-            /// <param name="count">
-            /// The maximum number of bytes to be read from the current stream.
-            /// </param>
-            /// <returns>
-            /// The total number of bytes read into the buffer. This can be less than the number of bytes requested 
-            /// if that many bytes are not currently available, or zero (0) if the end of the stream has been reached.
-            /// </returns>
-            /// <exception cref="T:System.ArgumentException">
-            /// The sum of <paramref name="offset"/> and <paramref name="count"/> is larger than the buffer length.
-            /// </exception>
-            /// <exception cref="T:System.ArgumentNullException">
-            ///     <paramref name="buffer"/> is null.
-            /// </exception>
-            /// <exception cref="T:System.ArgumentOutOfRangeException">
-            ///     <paramref name="offset"/> or <paramref name="count"/> is negative.
-            /// </exception>
-            /// <exception cref="T:System.IO.IOException">
-            /// An I/O error occurs.
-            /// </exception>
-            /// <exception cref="T:System.NotSupportedException">
-            /// The stream does not support reading.
-            /// </exception>
-            /// <exception cref="T:System.ObjectDisposedException">
-            /// Methods were called after the stream was closed.
-            /// </exception>
-            public override int Read(byte[] buffer, int offset, int count)
-            {
-                return this.sink.Read(buffer, offset, count);
-            }
-
-            /// <summary>
-            /// When overridden in a derived class, sets the position within the current stream.
-            /// </summary>
-            /// <param name="offset">A byte offset relative to the <paramref name="origin"/> parameter.</param>
-            /// <param name="origin">A value of type <see cref="T:System.IO.SeekOrigin"/> indicating the reference point used to obtain the new position.</param>
-            /// <returns>
-            /// The new position within the current stream.
-            /// </returns>
-            /// <exception cref="T:System.IO.IOException">
-            /// An I/O error occurs.
-            /// </exception>
-            /// <exception cref="T:System.NotSupportedException">
-            /// The stream does not support seeking, such as if the stream is constructed from a pipe or console output.
-            /// </exception>
-            /// <exception cref="T:System.ObjectDisposedException">
-            /// Methods were called after the stream was closed.
-            /// </exception>
-            public override long Seek(long offset, SeekOrigin origin)
-            {
-                return this.sink.Seek(offset, origin);
-            }
-
-            /// <summary>
-            /// When overridden in a derived class, sets the length of the current stream.
-            /// </summary>
-            /// <param name="value">The desired length of the current stream in bytes.</param>
-            /// <exception cref="T:System.IO.IOException">
-            /// An I/O error occurs.
-            /// </exception>
-            /// <exception cref="T:System.NotSupportedException">
-            /// The stream does not support both writing and seeking, such as if the stream is constructed from a pipe or console output.
-            /// </exception>
-            /// <exception cref="T:System.ObjectDisposedException">
-            /// Methods were called after the stream was closed.
-            /// </exception>
-            public override void SetLength(long value)
-            {
-                this.sink.SetLength(value);
-            }
-
-            /// <summary>
-            /// When overridden in a derived class, writes a sequence of bytes to the current stream and advances the current position within this stream by the number of bytes written.
-            /// </summary>
-            /// <param name="buffer">An array of bytes. This method copies <paramref name="count"/> bytes from <paramref name="buffer"/> to the current stream.</param>
-            /// <param name="offset">The zero-based byte offset in <paramref name="buffer"/> at which to begin copying bytes to the current stream.</param>
-            /// <param name="count">The number of bytes to be written to the current stream.</param>
-            /// <exception cref="T:System.ArgumentException">
-            /// The sum of <paramref name="offset"/> and <paramref name="count"/> is greater than the buffer length.
-            /// </exception>
-            /// <exception cref="T:System.ArgumentNullException">
-            ///     <paramref name="buffer"/> is null.
-            /// </exception>
-            /// <exception cref="T:System.ArgumentOutOfRangeException">
-            ///     <paramref name="offset"/> or <paramref name="count"/> is negative.
-            /// </exception>
-            /// <exception cref="T:System.IO.IOException">
-            /// An I/O error occurs.
-            /// </exception>
-            /// <exception cref="T:System.NotSupportedException">
-            /// The stream does not support writing.
-            /// </exception>
-            /// <exception cref="T:System.ObjectDisposedException">
-            /// Methods were called after the stream was closed.
-            /// </exception>
-            public override void Write(byte[] buffer, int offset, int count)
-            {
-                var html = Encoding.UTF8.GetString(buffer);
-
-                var regex =
-                    new Regex(
-                        "<script\\s*src=\"((?=[^\"]*webresource.axd)[^\"]*)\"\\s*type=\"text/javascript\"[^>]*>[^<]*(?:</script>)?",
-                        RegexOptions.IgnoreCase);
-                html = regex.Replace(html, this.Evaluator);
-
-                var outdata = Encoding.UTF8.GetBytes(html);
-                this.sink.Write(outdata, 0, outdata.GetLength(0));
-            }
-
-            #endregion
-        }
     }
 }
