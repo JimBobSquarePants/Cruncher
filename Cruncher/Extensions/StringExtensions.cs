9d99091821be422830f38f0493dff9eda5b82fc8
James South <james_south@hotmail.com>
2013-09-09 13:44:57 +0100

    Better relative path verifier

--- Cruncher/Extensions/StringExtensions.cs
+++ Cruncher/Extensions/StringExtensions.cs
@@ -68,32 +68,10 @@ namespace Cruncher.Extensions
         /// <returns>True if the given string is a valid virtual path name</returns>
         public static bool IsValidVirtualPathName(this string expression)
         {
-            // Check the start of the string.
-            if (expression.StartsWith("~/", StringComparison.OrdinalIgnoreCase))
-            {
-                // Trim the first two characters and test the path.
-                expression = expression.Substring(2);
-                return expression.IsValidPathName();
-            }
+            Uri uri;
 
-            return false;
+            return Uri.TryCreate(expression, UriKind.Relative, out uri) && uri.IsWellFormedOriginalString();
         }
-
-        /// <summary>
-        /// Checks the string to see whether the value is a valid path name.
-        /// <see cref="http://stackoverflow.com/questions/62771/how-check-if-given-string-is-legal-allowed-file-name-under-windows/"/>
-        /// </summary>
-        /// <param name="expression">The <see cref="T:System.String">String</see> instance that this method extends.</param>
-        /// <returns>True if the given string is a valid path name</returns>
-        public static bool IsValidPathName(this string expression)
-        {
-            // Create a regex of invalid characters and test it.
-            string invalidPathNameChars = new string(Path.GetInvalidFileNameChars());
-            Regex regFixPathName = new Regex("[" + Regex.Escape(invalidPathNameChars) + "]");
-
-            return !regFixPathName.IsMatch(expression);
-        }
-
         #endregion
     }
 }
